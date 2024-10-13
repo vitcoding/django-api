532a345c8f88f3bbf6d17622dcd6a7798cec96cb
Vitaly_D <vitcode.mail@gmail.com>
2024-10-13 16:09:43 +0300

    Simple BaseListView (Filmwork) added

--- movies/api/v1/views.py
+++ movies/api/v1/views.py
@@ -1,10 +1,36 @@
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.core import serializers
+from django.db.models import Q
 from django.http import JsonResponse
-from django.views import View
+from django.views.generic.list import BaseListView
+
+from movies.models import Filmwork
 
 
-class MoviesListApi(View):
-    http_method_names = ["get"]
+class MoviesListApi(BaseListView):
+    model = Filmwork
+    http_method_names = ["get"]  # Список методов, которые реализует обработчик
 
-    def get(self, request, *args, **kwargs):
-        # Получение и обработка данных
-        return JsonResponse({})
+    def get_queryset(self):
+        queryset = Filmwork.objects.all().values(
+            "id",
+            "title",
+            "description",
+            "creation_date",
+            "rating",
+            "type",
+            "genres",
+            # "actors",
+            # "directors",
+            # "writers",
+        )
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {
+            "results": list(self.get_queryset()),
+        }
+        return context
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
